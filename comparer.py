b0e34611b5a2e2baddf997d24b448e2a4cffbf00
Shashank Kumar Shankar <shank7485@users.noreply.github.com>
2016-01-28 00:22:10 -0600

    Update comparer.py

--- comparer.py
+++ comparer.py
@@ -1,19 +1,46 @@
-import requests
+from uber_estimate import uber_estimate
+from geo_code import geo_location
 
-class geo_location:
-    "API_key is Google Maps API key"
-    def __init__(self, API_key):
-        self.API_key = API_key
+class comparer_address:
+    def __init__(self, from_address, to_address, geo_api_key, uber_api_key):
+        self.from_address = from_address
+        self.to_address = to_address
+        self.geo_api_key = geo_api_key
+        self.uber_api_key = uber_api_key
 
-    def geo_locate(self, address):
-        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key" + str(self.API_key)
+    def services_prices(self):
 
-        response = requests.get(url)
-        data = response.json()["results"][0]["geometry"]
+        geo = geo_location(self.geo_api_key)
 
-        coodinates = data["location"]
+        (from_lat, from_long) = geo.geo_locate(self.from_address)
+        (to_lat, to_long) = geo.geo_locate(self.to_address)
 
-        lat = coodinates["lat"]
-        long = coodinates["lng"]
+        uber = uber_estimate(self.uber_api_key, from_lat, from_long, to_lat, to_long)
 
-        return (lat,long)
+        prices = uber.estimate()["prices"]
+
+        dct = {}
+        for name in prices:
+            dct.update({name["display_name"] : name["estimate"]})
+
+        return dct
+
+class comparer_coord:
+    def __init__(self, from_lat, from_long, to_lat, to_long, uber_api_key):
+        self.from_lat = from_lat
+        self.from_long = from_long
+        self.to_lat = to_lat
+        self.to_long = to_long
+        self.uber_api_key = uber_api_key
+
+    def services_prices(self):
+
+        uber = uber_estimate(self.uber_api_key, self.from_lat, self.from_long, self.to_lat, self.to_long)
+
+        prices = uber.estimate()["prices"]
+
+        dct = {}
+        for name in prices:
+            dct.update({name["display_name"] : name["estimate"]})
+
+        return dct
